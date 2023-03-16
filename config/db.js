80874d29f195d74061dd6abdd0daf8ca4753e07e
Hawkeye <36890222+capmahin@users.noreply.github.com>
2023-03-17 02:53:25 +0600

    export default connectDB

--- config/db.js
+++ config/db.js
@@ -4,8 +4,11 @@
  const connectDB = async () =>{
     try {
         const conn = await mongoose.connect(process.env.MONGO_URL)
+        console.log(`Connected To Mongodb Database ${conn.connection.host}`)
     } catch (error) {
         console.log(`Error in Mongodb ${error}
         `.bgRed.black);
     }
- }
+ };
+
+ export default connectDB;
