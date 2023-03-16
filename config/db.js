8b7cb58b7f21ac3aca710687407f55b6ae2f0260
Hawkeye <36890222+capmahin@users.noreply.github.com>
2023-03-17 02:47:55 +0600

    add try catch block

--- config/db.js
+++ config/db.js
@@ -1,1 +1,10 @@
  import mongoose from "mongoose";
+ import colors from 'colors';
+
+ const connectDB = async () =>{
+    try {
+        
+    } catch (error) {
+        console.log(error);
+    }
+ }
