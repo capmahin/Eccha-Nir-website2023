f8d440befc873ece55408a8256e6ffbc3576d30d
Hawkeye <36890222+capmahin@users.noreply.github.com>
2023-03-17 02:42:18 +0600

    connect to MONGO_DB

--- server.js
+++ server.js
@@ -17,5 +17,5 @@ const PORT = process.env.PORT || 8080;
 
 //run listen
 app.listen(PORT, ()=>{
-    console.log(`Server Running on ${process.env.DEV_MODE} mode on port ${PORT}`.bgCyan.white);
+    console.log(`Server Running on ${process.env.DEV_MODE} mode on port ${PORT}`.bgGreen.black);
 });
