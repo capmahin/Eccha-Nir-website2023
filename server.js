edda262ba8cc8ad9f638f1178d86ce2bd488b620
Hawkeye <36890222+capmahin@users.noreply.github.com>
2023-03-16 13:12:08 +0600

    add app.listen

--- server.js
+++ server.js
@@ -5,5 +5,15 @@ const app = express()
 
 //rest api
 app.get('/',(req,res)=>{
+    res.send({
+        message:'Welcome to ecommarce app'
+    })
+});
 
-})
+//PORT
+const PORT = 8080;
+
+//run listen
+app.listen(PORT, ()=>{
+    console.log(`Server Running on ${PORT}`);
+});
