faa323c43d14471232b1b0ff0c246cae29cba3fe
Hawkeye <36890222+capmahin@users.noreply.github.com>
2023-03-17 23:58:11 +0600

    add try catch and work on catch

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,13 @@
  export const registerController = async(req,res) =>{
-
+    try {
+        
+    } catch (error) {
+        console.log(error)
+        res.status(500).send({
+            success:false,
+            error,
+            message:'Error in Registeration',
+        })
+    }
 }
 
