3afa76f644580c9dcef596bda45e7aa69208ed45
Hawkeye <36890222+capmahin@users.noreply.github.com>
2023-03-17 23:34:23 +0600

    pass saltRounds and password in hashedpassword

--- helpers/authHelper.js
+++ helpers/authHelper.js
@@ -2,7 +2,8 @@ import bcrypt from 'bcrypt';
 
 export const hashPassword = async (password) =>{
     try {
-        
+        const saltRounds = 10
+        const hashedPassword = await bcrypt.hash(password, saltRounds)
     } catch (error) {
         console.log(error)
     }
