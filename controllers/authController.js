db72bd438e05d40df63a5395be63d110a27d616c
Hawkeye <36890222+capmahin@users.noreply.github.com>
2023-03-18 00:02:42 +0600

    add if statement

--- controllers/authController.js
+++ controllers/authController.js
@@ -4,6 +4,22 @@
  export const registerController = async(req,res) =>{
     try {
         const {name,email,password,phone,address} = req.body
+        //validations
+        if(!name){
+            return res.send({error:'Name is Required'})
+        }
+        if(!email){
+            return res.send({error:'Email is Required'})
+        }
+        if(!password){
+            return res.send({error:'Password is Required'})
+        }
+        if(!phone){
+            return res.send({error:'Phone Number is Required'})
+        }
+        if(!address){
+            return res.send({error:'Address is Required'})
+        }
     } catch (error) {
         console.log(error)
         res.status(500).send({
