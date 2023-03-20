776633f504c5b8d5906942702738487267256378
Hawkeye <36890222+capmahin@users.noreply.github.com>
2023-03-21 02:35:37 +0600

    add decode

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -2,5 +2,5 @@ import JWT from 'jsonwebtoken';
 
 //Protected Routes token base
 export const requireSignIn = async (req,res,next) =>{
-    
+    const decode = JWT.verify(req.headers.authorization, process.env.JWT_SECRET)
 }
