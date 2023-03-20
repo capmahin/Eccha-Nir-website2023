25602620bf5afcb293129b3cccd355eec943cbe9
Hawkeye <36890222+capmahin@users.noreply.github.com>
2023-03-21 02:36:54 +0600

    add try catch block

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -2,5 +2,10 @@ import JWT from 'jsonwebtoken';
 
 //Protected Routes token base
 export const requireSignIn = async (req,res,next) =>{
-    const decode = JWT.verify(req.headers.authorization, process.env.JWT_SECRET)
+    try {
+        const decode = JWT.verify(req.headers.authorization, process.env.JWT_SECRET)
+        next()
+    } catch (error) {
+        console.log(error)
+    }
 }
