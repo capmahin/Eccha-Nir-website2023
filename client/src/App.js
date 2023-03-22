fb6fc21829d900a36831cb65c47f79edd52a54c4
Hawkeye <36890222+capmahin@users.noreply.github.com>
2023-03-23 02:57:40 +0600

    wrap by layout

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,15 @@
 
 import './App.css';
+import Layout from './components/Layout/Layout';
 
 function App() {
   return (
     <>
+    <Layout>
      <h1>Eccha Nir</h1>
+    </Layout>
     </>
+    
   );
 }
 
