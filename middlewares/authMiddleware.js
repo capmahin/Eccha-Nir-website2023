7c2196f6ba98e3b33f6295bdba2ac58fd396c79c
Hawkeye <36890222+capmahin@users.noreply.github.com>
2023-03-23 02:08:24 +0600

    add decode in requireSigniN

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -4,7 +4,8 @@ import userModel from "../models/userModel.js";
 //Protected Routes token base
 export const requireSignIn = async (req,res,next) =>{
     try {
-        const decode = JWT.verify(req.headers.authorization, process.env.JWT_SECRET)
+        const decode = JWT.verify(req.headers.authorization, process.env.JWT_SECRET);
+        req.user = decode;
         next()
     } catch (error) {
         console.log(error)
