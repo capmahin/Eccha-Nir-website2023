5fd9f852be2aea0690ac708591d73fe66afaa109
Hawkeye <36890222+capmahin@users.noreply.github.com>
2023-03-23 04:13:14 +0600

    add more

--- client/src/components/Layout/Footer.js
+++ client/src/components/Layout/Footer.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>
-        <h1>footer comp</h1>
+    <div className="bg-success text-dark p-3">
+        <h4 className="text-center">All Right Reserved &copy; Eccha-Nir</h4>
     </div>
   )
 }
