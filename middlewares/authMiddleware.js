aad1397a143fbe4898b758ae7edf24d9b2160626
Hawkeye <36890222+capmahin@users.noreply.github.com>
2023-03-22 19:10:45 +0600

    add unAuthorize access

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -14,7 +14,13 @@ export const requireSignIn = async (req,res,next) =>{
 //admin access
 export const isAdmin = async(req,res,next) =>{
     try {
-        const user = await userModel.findById()
+        const user = await userModel.findById(req.user._id)
+        if(user.role !== 1){
+            return res.status(401).send({
+                success:false,
+                message:'UnAuthorized Access',
+            })
+        }
     } catch (error) {
         console.log(error)
     }
