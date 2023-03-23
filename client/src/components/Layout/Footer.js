28a7dfb061723d4e4ac90adb607eed21b1a48ee9
Hawkeye <36890222+capmahin@users.noreply.github.com>
2023-03-24 03:30:47 +0600

    add

--- client/src/components/Layout/Footer.js
+++ client/src/components/Layout/Footer.js
@@ -1,38 +1,87 @@
 import React from 'react'
 import {  FaLinkedin,FaFacebook, FaInstagram, FaWhatsapp } from "react-icons/fa";
 import { HiOutlineMail } from "react-icons/hi";
-
+import {AiFillHome} from 'react-icons/ai'
+import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
     
-  <div className="bg-success text-center text-dark p-">
+  <div className="bg-success text-center text-dark ">
    
-    <div className="container p-2 pb-0">
-      {/* Section: Social media */}
-      <section className="mb-4">
-        {/* Facebook */}
-        <a className="btn btn-outline-dark btn-floating m-1" href="#!" role="button"><FaLinkedin/></a>
-        {/* Twitter */}
-        <a className="btn btn-outline-dark btn-floating m-1" href="#!" role="button"><FaFacebook/></a>
-        {/* Google */}
-        <a className="btn btn-outline-dark btn-floating m-1" href="#!" role="button"><FaInstagram/></a>
-        {/* Instagram */}
-        <a className="btn btn-outline-dark btn-floating m-1" href="#!" role="button"><FaWhatsapp/></a>
-        {/* Linkedin */}
-        <a className="btn btn-outline-dark btn-floating m-1" href="#!" role="button"><HiOutlineMail/></a>
-        
+  <div className=" text-center text-lg-start" >
+  {/* Grid container */}
+  <div className="container p-1">
+    {/*Grid row*/}
+    <div className="row mt-3">
+      {/*Grid column*/}
+      <div className="col-lg-4 col-md-12 mb-4 mb-md-0">
+        <h5 className="text-uppercase mb-4">About company</h5>
+        <p>
+          At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium
+          voluptatum deleniti atque corrupti.
+        </p>
+        <p>
+          Blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas
+          molestias.
+        </p>
+        <div className="mt-1 text-dark">
+          {/* Facebook */}
+          <a type="button" className="btn btn-outline-light btn-floating btn-lg m-1"><FaLinkedin/></a>
+          {/* Dribbble */}
+          <a type="button" className="btn btn-outline-light btn-floating btn-lg m-1"><FaFacebook/></a>
+          {/* Twitter */}
+          <a type="button" className="btn btn-outline-light btn-floating btn-lg m-1"><FaInstagram /></a>
+          {/* Google + */}
+          <a type="button" className="btn btn-outline-light btn-floating btn-lg m-1"><FaWhatsapp /></a>
+          <a type="button" className="btn btn-outline-light btn-floating btn-lg m-1"><HiOutlineMail /></a>
+          {/* Linkedin */}
+        </div>
+      </div>
+      {/*Grid column*/}
+      {/*Grid column*/}
+      <div className="col-lg-4 col-md-6 mb-4 mb-md-0">
+        <h5 className="text-uppercase mb-4 pb-1">Search something</h5>
+        <div className="form-outline form-white mb-4">
+          <input type="text" id="formControlLg" className="form-control form-control-lg" />
+          <label className="form-label" htmlFor="formControlLg" style={{marginLeft: 0}}>Search</label>
+          <div className="form-notch"><div className="form-notch-leading" style={{width: 9}} /><div className="form-notch-middle" style={{width: '48.8px'}} /><div className="form-notch-trailing" /></div></div>
        
-      </section>
-      {/* Section: Social media */}
+      </div>
+      {/*Grid column*/}
+      {/*Grid column*/}
+      <div className="col-lg-4 col-md-6 mb-4 mb-md-0">
+        <h5 className="text-uppercase mb-4">Opening hours</h5>
+        <table className="table text-center text-dark">
+          <tbody className="font-weight-normal">
+            <tr>
+              <td>Mon - Thu:</td>
+              <td>8am - 9pm</td>
+            </tr>
+            <tr>
+              <td>Fri - Sat:</td>
+              <td>8am - 1am</td>
+            </tr>
+            <tr>
+              <td>Sunday:</td>
+              <td>9am - 10pm</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+      {/*Grid column*/}
     </div>
+    {/*Grid row*/}
+  </div>
+  {/* Grid container */}
+  {/* Copyright */}
+  <div className="text-center p-1" style={{backgroundColor: 'rgba(0, 0, 0, 0.2)'}}>
     
-  
-    <div className="text-center p-1" >
-      
-      <h4 className="text-center">All Right Reserved &copy; Eccha-Nir</h4> 
-    </div>
-   
+    <p>© 2020 Copyright:Eccha-Nir</p>
+  </div>
+  {/* Copyright */}
+</div>
+
 
         
   </div>
--- client/src/components/Layout/Header.js
+++ client/src/components/Layout/Header.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { NavLink,Link } from "react-router-dom"
+import {AiFillShopping} from 'react-icons/ai'
 
 const Header = () => {
   return (
@@ -10,7 +11,7 @@ const Header = () => {
       <span className="navbar-toggler-icon" />
     </button>
     <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
-      <Link to="/" className="navbar-brand fs-2" >Eccha Nir</Link>
+      <Link to="/" className="navbar-brand fs-2" ><AiFillShopping/> Eccha Nir</Link>
       <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
         <li className="nav-item">
           <NavLink to="/"  className="nav-link " >Home</NavLink>
