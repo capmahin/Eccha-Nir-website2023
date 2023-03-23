37ad86dc20389a0e4746e842064d9a988a1b2137
Hawkeye <36890222+capmahin@users.noreply.github.com>
2023-03-24 02:35:05 +0600

    add navbar manu

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,10 @@
 
 import {Routes,Route} from 'react-router-dom'
+import About from "./pages/About";
+import Contact from "./pages/Contact";
 import HomePage from "./pages/HomePage";
+import Pagenotfound from "./pages/Pagenotfound";
+import Policy from "./pages/Policy";
 
 
 function App() {
@@ -8,6 +12,10 @@ function App() {
     <>
       <Routes>
         <Route path="/"  element={<HomePage/>}/>
+        <Route path="/about"  element={<About/>}/>
+        <Route path="/contact"  element={<Contact/>}/>
+        <Route path="/policy"  element={<Policy/>}/>
+        <Route path="*"  element={<Pagenotfound/>}/>
       </Routes>
     </>
     
