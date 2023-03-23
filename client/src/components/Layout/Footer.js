1104bc0c517b95933a31d53a1bc08a138ef14ce5
Hawkeye <36890222+capmahin@users.noreply.github.com>
2023-03-24 01:10:52 +0600

    add footer

--- client/src/components/Layout/Footer.js
+++ client/src/components/Layout/Footer.js
@@ -1,10 +1,51 @@
 import React from 'react'
+import {  FaLinkedin,FaFacebook, FaInstagram, FaWhatsapp } from "react-icons/fa";
+import { HiOutlineMail } from "react-icons/hi";
+
 
 const Footer = () => {
   return (
-    <div className="bg-success text-dark p-3">
-        <h4 className="text-center">All Right Reserved &copy; Eccha-Nir</h4>
+    
+  <div className="bg-success text-center text-dark p-3">
+    {/* Grid container */}
+    <div className="container p-4 pb-0">
+      {/* Section: Social media */}
+      <section className="mb-4">
+        {/* Facebook */}
+        <a className="btn btn-outline-dark btn-floating m-1" href="#!" role="button"><FaLinkedin/></a>
+        {/* Twitter */}
+        <a className="btn btn-outline-dark btn-floating m-1" href="#!" role="button"><FaFacebook/></a>
+        {/* Google */}
+        <a className="btn btn-outline-dark btn-floating m-1" href="#!" role="button"><FaInstagram/></a>
+        {/* Instagram */}
+        <a className="btn btn-outline-dark btn-floating m-1" href="#!" role="button"><FaWhatsapp/></a>
+        {/* Linkedin */}
+        <a className="btn btn-outline-dark btn-floating m-1" href="#!" role="button"><HiOutlineMail/></a>
+        
+       
+      </section>
+      {/* Section: Social media */}
     </div>
+    {/* Grid container */}
+    {/* Copyright */}
+    <div className="text-center p-3" style={{backgroundColor: 'rgba(0, 0, 0, 0.2)'}}>
+      © 2020 Copyright:
+      <h4 className="text-center">All Right Reserved &copy; Eccha-Nir</h4> 
+    </div>
+    {/* Copyright */}
+     {/* Footer */}
+
+        {/* */}
+  </div>
+
+
+
+ 
+    
+    
+
+
+
   )
 }
 
