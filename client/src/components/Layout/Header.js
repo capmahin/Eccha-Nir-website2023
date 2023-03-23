c6ee7239590e7a79c3f064910f7bb85602cdd74b
Hawkeye <36890222+capmahin@users.noreply.github.com>
2023-03-24 02:40:12 +0600

    add login and resisgter

--- client/src/components/Layout/Header.js
+++ client/src/components/Layout/Header.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { NavLink } from "react-router-dom"
 
 const Header = () => {
   return (
@@ -9,22 +10,20 @@ const Header = () => {
       <span className="navbar-toggler-icon" />
     </button>
     <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
-      <a className="navbar-brand" href="#">Hidden brand</a>
+      <NavLink to="/" className="navbar-brand" href="#">Hidden brand</NavLink>
       <ul className="navbar-nav me-auto mb-2 mb-lg-0">
         <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="#">Home</a>
+          <NavLink to="/"  className="nav-link active" aria-current="page" href="#">Home</NavLink>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="#">Link</a>
+          <NavLink to="/resister"  className="nav-link" href="#">Register</NavLink>
         </li>
         <li className="nav-item">
-          <a className="nav-link disabled">Disabled</a>
+          <NavLink to="/login"  className="nav-link" href="#">Login</NavLink>
         </li>
+        
       </ul>
-      <form className="d-flex" role="search">
-        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-        <button className="btn btn-outline-success" type="submit">Search</button>
-      </form>
+      
     </div>
   </div>
 </nav>
