ff7976d2d8af3f893e63f828103fa674567eb14c
Hawkeye <36890222+capmahin@users.noreply.github.com>
2023-03-25 02:57:54 +0600

    add

--- client/src/components/Layout/Layout.js
+++ client/src/components/Layout/Layout.js
@@ -1,10 +1,18 @@
 import React from 'react'
 import Footer from "./Footer"
 import Header from "./Header"
+import {Helmet} from "react-helmet";
 
-const Layout = ({children}) => {
+const Layout = ({children,title,description,keywords,author}) => {
   return (
     <div>
+      <Helmet>
+                <meta charSet="utf-8" />
+                <title>My Title</title>              
+  <meta name="description" content="Free Web tutorials" />
+  <meta name="keywords" content="HTML, CSS, JavaScript" />
+  <meta name="author" content="John Doe" />        
+            </Helmet>
         <Header/>
         <main style={{minHeight:"80vh"}}>
         {children}
