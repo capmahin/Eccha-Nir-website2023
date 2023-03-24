f289519ee285344f19bb59bfc22781ef1e543dae
Hawkeye <36890222+capmahin@users.noreply.github.com>
2023-03-25 02:43:27 +0600

    add pagenotfound

--- client/src/pages/Pagenotfound.js
+++ client/src/pages/Pagenotfound.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import { Link } from "react-router-dom"
 import Layout from "../components/Layout/Layout"
 
 const Pagenotfound = () => {
   return (
-    <Layout><h1>404</h1></Layout>
+    <Layout><div className="pnf">
+      <h1 className="pnf-title">404</h1>
+      <h2>Oops ! page not found</h2>
+      <Link to='/' className="pnf-btn">Go Back</Link>
+      </div></Layout>
   )
 }
 
