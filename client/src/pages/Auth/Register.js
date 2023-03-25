71812e6e96f806b2a1edab4a64614589878bcfd9
Hawkeye <36890222+capmahin@users.noreply.github.com>
2023-03-26 01:39:49 +0600

    add placeholder

--- client/src/pages/Auth/Register.js
+++ client/src/pages/Auth/Register.js
@@ -8,28 +8,29 @@ const Register = () => {
             <h1>Register Page</h1>
             <form>
   <div className="mb-3">
-    <label htmlFor="exampleInputName" className="form-label">name</label>
-    <input type="text" className="form-control" id="exampleInputName" />
+    
+    <input type="text" className="form-control" id="exampleInputName"
+    placeholder="Enter Your Name" />
     
   </div>
   <div className="mb-3">
-    <label htmlFor="exampleInputEmail" className="form-label">Email</label>
-    <input type="email" className="form-control" id="exampleInputEmail" />
+    
+    <input type="email" className="form-control" id="exampleInputEmail" placeholder="Enter Your Email"/>
     
   </div>
   <div className="mb-3">
-    <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
-    <input type="password" className="form-control" id="exampleInputPassword1" />
+    
+    <input type="password" className="form-control" id="exampleInputPassword" placeholder="Enter Your Password" />
   </div>
 
   <div className="mb-3">
-    <label htmlFor="exampleInputPhone" className="form-label">Phone</label>
-    <input type="text" className="form-control" id="exampleInputPhone" />
+    
+    <input type="text" className="form-control" id="exampleInputPhone" placeholder="Enter Your Phone Number"/>
     
   </div>
   <div className="mb-3">
-    <label htmlFor="exampleInputAddress" className="form-label">Address</label>
-    <input type="text" className="form-control" id="exampleInputAddress" />
+    
+    <input type="text" className="form-control" id="exampleInputAddress" placeholder="Enter Your Address" />
     
   </div>
   <button type="submit" className="btn btn-primary">Submit</button>
