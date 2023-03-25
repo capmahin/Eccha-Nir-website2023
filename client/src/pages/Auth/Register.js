a22af60cd6f9e472a9d1932fc8755740dc3b7045
Hawkeye <36890222+capmahin@users.noreply.github.com>
2023-03-26 01:45:40 +0600

    add values in input field

--- client/src/pages/Auth/Register.js
+++ client/src/pages/Auth/Register.js
@@ -14,28 +14,28 @@ const Register = () => {
             <form>
   <div className="mb-3">
     
-    <input type="text" className="form-control" id="exampleInputName"
+    <input type="text" value={name} className="form-control" id="exampleInputName"
     placeholder="Enter Your Name" />
     
   </div>
   <div className="mb-3">
     
-    <input type="email" className="form-control" id="exampleInputEmail" placeholder="Enter Your Email"/>
+    <input type="email" value={email} className="form-control" id="exampleInputEmail" placeholder="Enter Your Email"/>
     
   </div>
   <div className="mb-3">
     
-    <input type="password" className="form-control" id="exampleInputPassword" placeholder="Enter Your Password" />
+    <input type="password" value={password} className="form-control" id="exampleInputPassword" placeholder="Enter Your Password" />
   </div>
 
   <div className="mb-3">
     
-    <input type="text" className="form-control" id="exampleInputPhone" placeholder="Enter Your Phone Number"/>
+    <input type="text" value={phone} className="form-control" id="exampleInputPhone" placeholder="Enter Your Phone Number"/>
     
   </div>
   <div className="mb-3">
     
-    <input type="text" className="form-control" id="exampleInputAddress" placeholder="Enter Your Address" />
+    <input type="text" value={address} className="form-control" id="exampleInputAddress" placeholder="Enter Your Address" />
     
   </div>
   <button type="submit" className="btn btn-primary">Submit</button>
