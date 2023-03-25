1c2905de28003b91e4c51dbde5df7d24c95e8ad0
Hawkeye <36890222+capmahin@users.noreply.github.com>
2023-03-26 01:33:07 +0600

    add more option

--- client/src/pages/Auth/Register.js
+++ client/src/pages/Auth/Register.js
@@ -8,9 +8,9 @@ const Register = () => {
             <h1>Register Page</h1>
             <form>
   <div className="mb-3">
-    <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
-    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
-    <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
+    <label htmlFor="exampleInputName" className="form-label">name</label>
+    <input type="text" className="form-control" id="exampleInputEmail1" />
+    
   </div>
   <div className="mb-3">
     <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
