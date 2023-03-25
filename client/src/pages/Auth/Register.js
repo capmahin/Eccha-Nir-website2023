ad63473e6a7407da20d14c65f0bcb79ecf9863f5
Hawkeye <36890222+capmahin@users.noreply.github.com>
2023-03-26 01:52:19 +0600

    add required for all input

--- client/src/pages/Auth/Register.js
+++ client/src/pages/Auth/Register.js
@@ -15,27 +15,27 @@ const Register = () => {
   <div className="mb-3">
     
     <input type="text" value={name} onChange={(e)=> setName(e.target.value)} className="form-control" id="exampleInputName"
-    placeholder="Enter Your Name" />
+    placeholder="Enter Your Name" required />
     
   </div>
   <div className="mb-3">
     
-    <input type="email" value={email}onChange={(e)=> setEmail(e.target.value)} className="form-control" id="exampleInputEmail" placeholder="Enter Your Email"/>
+    <input type="email" value={email}onChange={(e)=> setEmail(e.target.value)} className="form-control" id="exampleInputEmail" placeholder="Enter Your Email" required/>
     
   </div>
   <div className="mb-3">
     
-    <input type="password" value={password}  onChange={(e)=> setPassword(e.target.value)} className="form-control" id="exampleInputPassword" placeholder="Enter Your Password" />
+    <input type="password" value={password}  onChange={(e)=> setPassword(e.target.value)} className="form-control" id="exampleInputPassword" placeholder="Enter Your Password" required/>
   </div>
 
   <div className="mb-3">
     
-    <input type="text" value={phone} onChange={(e)=> setPhone(e.target.value)} className="form-control" id="exampleInputPhone" placeholder="Enter Your Phone Number"/>
+    <input type="text" value={phone} onChange={(e)=> setPhone(e.target.value)} className="form-control" id="exampleInputPhone" placeholder="Enter Your Phone Number" required/>
     
   </div>
   <div className="mb-3">
     
-    <input type="text" value={address} onChange={(e)=> setAddress(e.target.value)} className="form-control" id="exampleInputAddress" placeholder="Enter Your Address" />
+    <input type="text" value={address} onChange={(e)=> setAddress(e.target.value)} className="form-control" id="exampleInputAddress" placeholder="Enter Your Address" required/>
     
   </div>
   <button type="submit" className="btn btn-primary">Submit</button>
