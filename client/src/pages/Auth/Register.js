17dd5633dcde533ad52da029f913526fe5c2df9a
Hawkeye <36890222+capmahin@users.noreply.github.com>
2023-03-26 01:37:12 +0600

    add more feild

--- client/src/pages/Auth/Register.js
+++ client/src/pages/Auth/Register.js
@@ -9,16 +9,28 @@ const Register = () => {
             <form>
   <div className="mb-3">
     <label htmlFor="exampleInputName" className="form-label">name</label>
-    <input type="text" className="form-control" id="exampleInputEmail1" />
+    <input type="text" className="form-control" id="exampleInputName" />
+    
+  </div>
+  <div className="mb-3">
+    <label htmlFor="exampleInputEmail" className="form-label">Email</label>
+    <input type="email" className="form-control" id="exampleInputEmail" />
     
   </div>
   <div className="mb-3">
     <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
     <input type="password" className="form-control" id="exampleInputPassword1" />
   </div>
-  <div className="mb-3 form-check">
-    <input type="checkbox" className="form-check-input" id="exampleCheck1" />
-    <label className="form-check-label" htmlFor="exampleCheck1">Check me out</label>
+
+  <div className="mb-3">
+    <label htmlFor="exampleInputPhone" className="form-label">Phone</label>
+    <input type="text" className="form-control" id="exampleInputPhone" />
+    
+  </div>
+  <div className="mb-3">
+    <label htmlFor="exampleInputAddress" className="form-label">Address</label>
+    <input type="text" className="form-control" id="exampleInputAddress" />
+    
   </div>
   <button type="submit" className="btn btn-primary">Submit</button>
 </form>
