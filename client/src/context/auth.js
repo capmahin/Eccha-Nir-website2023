528b93a66270c64b3ea6186f80488926ba1680dc
Hawkeye <36890222+capmahin@users.noreply.github.com>
2023-03-28 01:26:11 +0600

    add authProvider

--- client/src/context/auth.js
+++ client/src/context/auth.js
@@ -1,1 +1,18 @@
-import { useState,useEffect,useContext } from "react";
+import { useState,useEffect,useContext,createContext } from "react";
+
+const AuthContext = createContext()
+
+
+
+const AuthProvider = ({children}) =>{
+
+    const [auth,setAuth] = useState({
+        user:null,
+        token:""
+    })
+    return(
+        <AuthContext.Provider>
+            {children}
+        </AuthContext.Provider>
+    )
+}
