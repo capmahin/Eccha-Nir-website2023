afa5a4805c7b9c10eac9e292f84a216766c6b9de
Hawkeye <36890222+capmahin@users.noreply.github.com>
2023-03-30 01:36:08 +0600

    add outlate

--- client/src/components/Routes/Private.js
+++ client/src/components/Routes/Private.js
@@ -1,6 +1,12 @@
 import { useState,useEffect } from "react";
 import { useAuth } from "../../context/auth";
+import { Outlet } from "react-router-dom";
 
 export default function PrivateRoute(){
-    const [ok,setOk] = useState(false) 
+    const [ok,setOk] = useState(false)
+    const [auth,setAuth] = useAuth()
+    
+    useEffect(()=>{},[])
+
+    return ok ? <Outlet/> : 'spinner'
 }
