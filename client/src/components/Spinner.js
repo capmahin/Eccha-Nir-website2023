402df5e51b2cb94bd63f3b4bd2b1fc9d0516f869
Hawkeye <36890222+capmahin@users.noreply.github.com>
2023-03-30 02:12:31 +0600

    add useEffect and useState

--- client/src/components/Spinner.js
+++ client/src/components/Spinner.js
@@ -1,10 +1,17 @@
-import React from 'react'
+import React,{useEffect,useState} from 'react';
+import { useNavigate } from "react-router-dom";
 
 const Spinner = () => {
+    const [count,setCount] = useState(5)
+    const navigate = useNavigate()
+
+    useEffect(()=>{
+        
+    },[])
   return (
     <>
-    <div className="d-flex justify-content-center align-items-center" style={{height:'100vh'}}>
-  <div className="spinner-grow text-success" role="status">
+    <div className="d-flex justify-content-center align-items-center bg-success" style={{height:'100vh'}}>
+  <div className="spinner-grow text-dark" role="status">
     <span className="visually-hidden">Loading...</span>
   </div>
 </div>
