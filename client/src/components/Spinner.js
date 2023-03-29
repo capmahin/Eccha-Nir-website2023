282af30a17c0804fd8790a7a081e8edecc80f757
Hawkeye <36890222+capmahin@users.noreply.github.com>
2023-03-30 01:58:34 +0600

    add Spinner

--- client/src/components/Spinner.js
+++ client/src/components/Spinner.js
@@ -2,7 +2,14 @@ import React from 'react'
 
 const Spinner = () => {
   return (
-    <div>Spinner</div>
+    <>
+    <div className="d-flex justify-content-center">
+  <div className="spinner-grow text-success" role="status">
+    <span className="visually-hidden">Loading...</span>
+  </div>
+</div>
+
+    </>
   )
 }
 
