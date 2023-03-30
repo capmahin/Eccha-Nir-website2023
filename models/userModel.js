87348677233d9892833efb132eaa6e70f58cc41e
Hawkeye <36890222+capmahin@users.noreply.github.com>
2023-03-31 01:05:06 +0600

    add aditional quiestion

--- models/userModel.js
+++ models/userModel.js
@@ -25,6 +25,10 @@ const userSchema = new mongoose.Schema({
         required:true,
 
     },
+    question:{
+        type:String,
+        required:true
+    },
     role:{
         type:Number,
         default:0
