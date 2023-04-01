41c6485d03a5aac6f63a5afa8600674f27d1f477
Hawkeye <36890222+capmahin@users.noreply.github.com>
2023-04-02 01:31:23 +0600

    add layout in admin dashboard

--- client/src/pages/Admin/AdminDashboard.js
+++ client/src/pages/Admin/AdminDashboard.js
@@ -1,8 +1,11 @@
 import React from 'react'
+import Layout from './../../components/Layout/Layout';
 
 const AdminDashboard = () => {
   return (
-    <div>AdminDashboard</div>
+    <Layout><h1>
+        AdminDashboard
+        </h1></Layout>
   )
 }
 
