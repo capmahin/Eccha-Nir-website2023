7949b6b0852065b8f51e21060afc76499a8ec0c4
Hawkeye <36890222+capmahin@users.noreply.github.com>
2023-04-02 01:22:47 +0600

    add li "

--- client/src/components/Layout/Header.js
+++ client/src/components/Layout/Header.js
@@ -40,17 +40,18 @@ const Header = () => {
         ) : (
           <>
            <li className="nav-item dropdown">
-  <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-    Dropdown link
-  </a>
+  <NavLink className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+  {auth?.user?.name}
+  </NavLink>
   <ul className="dropdown-menu">
-    <li><NavLink to="/dashboard" className="dropdown-item" href="#">Action</NavLink ></li>
+    <li><NavLink to="/dashboard" className="dropdown-item" >Dashboard</NavLink ></li>
+    <li>
+    <NavLink onClick={handleLogout} to="/login"  className="nav-link" >LogOut</NavLink>
+    </li>
   </ul>
 </li>
 
-          <li className="nav-item">
-          <NavLink onClick={handleLogout} to="/login"  className="nav-link" >LogOut</NavLink>
-        </li>
+          
           </>
         )
        }
