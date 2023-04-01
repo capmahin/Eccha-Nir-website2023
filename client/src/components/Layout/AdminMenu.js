d8b3134fb7f4fdeb76a0b83610f298f9ebb8ed8b
Hawkeye <36890222+capmahin@users.noreply.github.com>
2023-04-02 02:08:11 +0600

    add navlink

--- client/src/components/Layout/AdminMenu.js
+++ client/src/components/Layout/AdminMenu.js
@@ -1,8 +1,21 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom';
 
 const AdminMenu = () => {
   return (
-    <div>AdminMenu</div>
+    <>
+    <div className="list-group">
+  <a href="#" className="list-group-item list-group-item-action active" aria-current="true">
+    The current link item
+  </a>
+  <a href="#" className="list-group-item list-group-item-action">A second link item</a>
+  <a href="#" className="list-group-item list-group-item-action">A third link item</a>
+  <a href="#" className="list-group-item list-group-item-action">A fourth link item</a>
+  <a className="list-group-item list-group-item-action disabled">A disabled link item</a>
+</div>
+
+
+    </>
   )
 }
 
