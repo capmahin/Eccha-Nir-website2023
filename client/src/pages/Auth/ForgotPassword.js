6d19176b533d300b49dbf42675cfed1fc91d4ae4
Hawkeye <36890222+capmahin@users.noreply.github.com>
2023-04-01 16:06:02 +0600

    add form in forgot password

--- client/src/pages/Auth/ForgotPassword.js
+++ client/src/pages/Auth/ForgotPassword.js
@@ -1,11 +1,70 @@
-import React from 'react'
-import Layout from "../../components/Layout/Layout"
+import React,{useState} from 'react'
+import Layout from './../../components/Layout/Layout';
+import  {toast} from 'react-hot-toast';
+import axios from 'axios';
+import { useNavigate,useLocation } from "react-router-dom";
+import "../../styles/AuthStyles.css";
+import { useAuth } from "../../context/auth";
 
 const ForgotPassword = () => {
+    const [auth,setAuth] = useAuth()
+    const [email,setEmail] = useState("");
+    const [password,setPassword] = useState("");
+   
+    const navigate = useNavigate();
+    const location = useLocation();
+
+     //form function
+
+     const handleSubmit = async (e) =>{
+        e.preventDefault();
+        try {
+            const res = await axios.post("/api/v1/auth/login",{
+                email,password
+            })
+            if(res && res.data.success){
+                toast.success(res.data.message);
+                setAuth({
+                  ...auth,
+                  user:res.data.user,
+                  token:res.data.token
+                });
+                localStorage.setItem('auth',JSON.stringify(res.data))
+                navigate(location.state ||"/");
+            }else{
+                toast.error(res.data.message)
+            }
+        } catch (error) {
+            console.log(error)
+            toast.error("Something went wrong")
+        }
+    }
   return (
-    <Layout><h1>
-        Forgot-Password
-        </h1></Layout>
+    <Layout title={'Forgot-Password- Eccha-nir'}> <div className="form-container"style={{ minHeight: "90vh" }} >
+            
+    <form onSubmit={handleSubmit}>
+    <h2 className="title">Reset Password</h2>
+
+<div className="mb-3">
+
+<input type="email" value={email}onChange={(e)=> setEmail(e.target.value)} className="form-control" id="exampleInputEmail" placeholder="Enter Your Email" required/>
+
+</div>
+<div className="mb-3">
+
+<input type="password" value={password}  onChange={(e)=> setPassword(e.target.value)} className="form-control" id="exampleInputPassword" placeholder="Enter Your Password" required/>
+</div>
+
+<div className="mb-3">
+<button type="button" className="btn btn-primary"
+onClick={()=>{navigate('/forgot-password')}}
+>Forgot Password</button>
+</div>
+
+<button type="submit" className="btn btn-primary">Login</button>
+</form>
+
+</div></Layout>
   )
 }
 
