fdff310389a3f32ee0582c80aae73619e1b9d4e6
Hawkeye <36890222+capmahin@users.noreply.github.com>
2023-04-02 16:47:37 +0600

    add and wrap by container

--- client/src/pages/Admin/CreateProduct.js
+++ client/src/pages/Admin/CreateProduct.js
@@ -5,6 +5,7 @@ import Layout from "../../components/Layout/Layout"
 const CreateProduct = () => {
   return (
     <Layout>
+      <div className="container-fluid m-3 p-3">
     <div className="row">
       <div className="col-md-3">
         <AdminMenu/>
@@ -12,6 +13,7 @@ const CreateProduct = () => {
       <div className="col-md-9">
         <h1>Create Product</h1>
       </div>
+    </div>
     </div>
   </Layout>
   )
--- client/src/pages/Admin/Users.js
+++ client/src/pages/Admin/Users.js
@@ -5,6 +5,7 @@ import AdminMenu from "../../components/Layout/AdminMenu"
 const Users = () => {
   return (
     <Layout>
+      <div className="container-fluid m-3 p-3">
       <div className="row">
         <div className="col-md-3">
           <AdminMenu/>
@@ -12,6 +13,7 @@ const Users = () => {
         <div className="col-md-9">
           <h1>All Users</h1>
         </div>
+      </div>
       </div>
     </Layout>
   )
