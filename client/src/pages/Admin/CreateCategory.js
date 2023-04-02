e8855dd7f872a7d71eb03a3ae158713e2717c415
Hawkeye <36890222+capmahin@users.noreply.github.com>
2023-04-02 16:43:44 +0600

    add and wrap

--- client/src/pages/Admin/CreateCategory.js
+++ client/src/pages/Admin/CreateCategory.js
@@ -1,8 +1,19 @@
 import React from 'react'
+import Layout from "../../components/Layout/Layout"
+import AdminMenu from "../../components/Layout/AdminMenu"
 
 const CreateCategory = () => {
   return (
-    <div><h1>Create Category</h1></div>
+    <Layout>
+      <div className="row">
+        <div className="col-md-3">
+          <AdminMenu/>
+        </div>
+        <div className="col-md-9">
+          <h1>Create Category</h1>
+        </div>
+      </div>
+    </Layout>
   )
 }
 
--- client/src/pages/Admin/CreateProduct.js
+++ client/src/pages/Admin/CreateProduct.js
@@ -1,8 +1,19 @@
 import React from 'react'
+import AdminMenu from "../../components/Layout/AdminMenu"
+import Layout from "../../components/Layout/Layout"
 
 const CreateProduct = () => {
   return (
-    <div><h1>Create Product</h1></div>
+    <Layout>
+    <div className="row">
+      <div className="col-md-3">
+        <AdminMenu/>
+      </div>
+      <div className="col-md-9">
+        <h1>Create Product</h1>
+      </div>
+    </div>
+  </Layout>
   )
 }
 
