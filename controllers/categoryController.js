ed50b9f3a2de481168a4de5ab441bf210099ec96
Hawkeye <36890222+capmahin@users.noreply.github.com>
2023-04-04 01:41:05 +0600

    add new categoryController

---
+++ controllers/categoryController.js
@@ -0,0 +1,7 @@
+export const createCategoryController = async (req,res) =>{
+    try {
+        
+    } catch (error) {
+        console.log(error)
+    }
+}
--- routes/categoryRoutes.js
+++ routes/categoryRoutes.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import { isAdmin, requireSignIn } from "../middlewares/authMiddleware.js"
+import { createCategoryController } from "../controllers/categoryController.js"
 
 const router = express.Router()
 
