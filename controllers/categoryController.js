ac11ccc46479db6048e1a3165cd335f2947e4791
Hawkeye <36890222+capmahin@users.noreply.github.com>
2023-04-04 01:53:59 +0600

    add more

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -1,8 +1,17 @@
+import categoryModel from "../models/categoryModel.js"
+
 export const createCategoryController = async (req,res) =>{
     try {
         const {name} = req.body
         if(!name){
             return res.status(401).send({message:'Name is required'})
+        }
+        const existingCategory = await categoryModel.findOne({name})
+        if(existingCategory){
+            return res.status(200).send({
+                success:true,
+                message:'Category Already Exist'
+            })
         }
     } catch (error) {
         console.log(error)
