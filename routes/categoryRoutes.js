0e32272352f19fefd6a56b2a79deb52fa0586a53
Hawkeye <36890222+capmahin@users.noreply.github.com>
2023-04-04 01:38:41 +0600

    add new routes for category

---
+++ routes/categoryRoutes.js
@@ -0,0 +1,7 @@
+import express from 'express'
+import { isAdmin, requireSignIn } from "../middlewares/authMiddleware.js"
+
+const router = express.Router()
+
+//routes
+router.post('create-category',requireSignIn,isAdmin, createCategoryController)
