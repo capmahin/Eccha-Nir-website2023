8b607ee0296e0156c7f63a3bf15c8271f8deb2de
Hawkeye <36890222+capmahin@users.noreply.github.com>
2023-04-05 05:32:01 +0600

    add

--- controllers/productController.js
+++ controllers/productController.js
@@ -3,6 +3,25 @@ import fs from 'fs';
 
 export const createProductController = async(req,res)=>{
     try {
+        const {name,slug,description,price,category,quantity,shipping} = req.fields;
+        const {photo} = req.files;
+        //validation
+        switch(true){
+            case !name:
+                return res.status(500).send({error:'Name required'})
+            case !slug:
+                return res.status(500).send({error:'Slug required'})
+            case !description:
+                return res.status(500).send({error:'Description required'})
+            case !price:
+                return res.status(500).send({error:'Price required'})
+            case !category:
+                return res.status(500).send({error:'Category required'})
+            case !quantity:
+                return res.status(500).send({error:'Quantity required'})
+            case !shipping:
+                return res.status(500).send({error:'Shipping required'})
+        }
         const products = await productModel
     } catch (error) {
         console.log(error)
