25b01193c78bda96459ec705adbc3f0aeb833dae
Hawkeye <36890222+capmahin@users.noreply.github.com>
2023-04-05 05:14:51 +0600

    add another

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,3 +1,12 @@
 export const createProductController = async(req,res)=>{
-    
+    try {
+        
+    } catch (error) {
+        console.log(error)
+        res.send(500).send({
+            success:false,
+            message:'Error in Create Product',
+            error,
+        })
+    }
 }
