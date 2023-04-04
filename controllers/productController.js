59dfdc70c65767a8e48b1c744185ef812da75ed0
Hawkeye <36890222+capmahin@users.noreply.github.com>
2023-04-05 05:22:21 +0600

    add express-formidble

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,6 +1,8 @@
+import productModel from "../models/productModel.js"
+
 export const createProductController = async(req,res)=>{
     try {
-        
+        const products = await productModel
     } catch (error) {
         console.log(error)
         res.send(500).send({
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import { isAdmin, requireSignIn } from "../middlewares/authMiddleware.js";
 import { createProductController } from "../controllers/productController.js";
+import formidable from 'express-formidable';
 
 const router = express.Router();
 
