589a2546d75f5d2644cd328c0f77ca9e50d0c23c
Hawkeye <36890222+capmahin@users.noreply.github.com>
2023-04-05 01:45:32 +0600

    add new

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -96,7 +96,12 @@ export const singleCategoryController = async(req,res)=>{
 
 export const deleteCategoryController = async(req,res)=>{
     try {
-        
+        const {id} = req.params;
+        await categoryModel.findByIdAndDelete(id);
+        res.status(200).send({
+            success:true,
+            message:'Category Deleted Successfully',
+        })
     } catch (error) {
         console.log(error)
         res.status(500).send({
