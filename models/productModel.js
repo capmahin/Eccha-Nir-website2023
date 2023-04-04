555cf3b43542334249a461bb76ecbcc25a7eefbf
Hawkeye <36890222+capmahin@users.noreply.github.com>
2023-04-05 04:42:21 +0600

    add new category and name

--- models/productModel.js
+++ models/productModel.js
@@ -2,6 +2,26 @@ import mongoose from "mongoose";
 
 const productSchema = new mongoose.Schema({
 
+    name:{
+        type:String,
+        required:true
+    },
+    slug:{
+        type:String,
+        required:true
+    },
+    description:{
+        type:String,
+        required:true
+    },
+    price:{
+        type:Number,
+        required:true
+    },
+    category:{
+        type:mongoose.ObjectId,
+        ref:'Category'
+    }
 })
 
 export default mongoose.model('Products', productSchema)
