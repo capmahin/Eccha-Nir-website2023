eed01b9ae52852d2c25ca1a0a503b160093c0e6f
Hawkeye <36890222+capmahin@users.noreply.github.com>
2023-04-06 02:08:39 +0600

    add more

--- controllers/productController.js
+++ controllers/productController.js
@@ -93,7 +93,11 @@ export const getSingleProductController = async(req,res)=>{
 
 export const productPhotoController = async(req,res)=>{
     try {
-        
+        const product = await productModel.findById(req.params.pid).select("photo");
+        if(product.photo.data){
+            res.set('Content-type',product.photo.contentType)
+            return res.status(200).send(product.photo.data)
+        }
     } catch (error) {
         console.log(error)
         res.status(500).send({
