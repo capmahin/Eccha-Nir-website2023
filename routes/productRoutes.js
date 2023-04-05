2e1bfd62a154f645c98340f1f69f0096e6baf12e
Hawkeye <36890222+capmahin@users.noreply.github.com>
2023-04-06 01:28:13 +0600

    add getProductController in routes

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import { isAdmin, requireSignIn } from "../middlewares/authMiddleware.js";
-import { createProductController } from "../controllers/productController.js";
+import { createProductController, getProductController } from "../controllers/productController.js";
 import formidable from 'express-formidable';
 
 const router = express.Router();
