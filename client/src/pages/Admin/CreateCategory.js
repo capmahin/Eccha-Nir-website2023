758bfd296872792717300ad5f10e47e594632d89
Hawkeye <36890222+capmahin@users.noreply.github.com>
2023-04-07 01:37:48 +0600

    add state

--- client/src/pages/Admin/CreateCategory.js
+++ client/src/pages/Admin/CreateCategory.js
@@ -1,8 +1,9 @@
-import React from 'react'
+import React,{useEffect,useState} from 'react'
 import Layout from "../../components/Layout/Layout"
 import AdminMenu from "../../components/Layout/AdminMenu"
 
 const CreateCategory = () => {
+  const [categories, setCategories] = useState([])
   return (
     <Layout title={'Dashboard-Create Category'}>
       <div className="container-fluid m-3 p-3">
