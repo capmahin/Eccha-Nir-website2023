d5be1ede6f57fc85dceb474b9400c862c45873d4
Hawkeye <36890222+capmahin@users.noreply.github.com>
2023-04-08 15:35:19 +0600

    add table in category page

--- client/src/pages/Admin/CreateCategory.js
+++ client/src/pages/Admin/CreateCategory.js
@@ -32,6 +32,37 @@ const CreateCategory = () => {
         </div>
         <div className="col-md-9">
           <h1>Create Category</h1>
+          <div>
+          <table className="table">
+  <thead>
+    <tr>
+     
+      <th scope="col">Name</th>
+      <th scope="col">Action</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Jacob</td>
+      <td>Thornton</td>
+      <td>@fat</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td colSpan={2}>Larry the Bird</td>
+      <td>@twitter</td>
+    </tr>
+  </tbody>
+</table>
+
+          </div>
         </div>
       </div>
       </div>
