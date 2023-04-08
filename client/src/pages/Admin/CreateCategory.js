770f4a543eb7ce5756d6e0fd57ae0ffeed040419
Hawkeye <36890222+capmahin@users.noreply.github.com>
2023-04-08 15:41:11 +0600

    add new

--- client/src/pages/Admin/CreateCategory.js
+++ client/src/pages/Admin/CreateCategory.js
@@ -12,7 +12,7 @@ const CreateCategory = () => {
     try {
       const {data} = await axios.get('/api/v1/category/get-category');
       if(data.success){
-        setCategories(data)
+        setCategories(data.category)
       }
     } catch (error) {
       console.log(error)
@@ -43,21 +43,11 @@ const CreateCategory = () => {
   </thead>
   <tbody>
     <tr>
-      <th scope="row">1</th>
-      <td>Mark</td>
-      <td>Otto</td>
-      <td>@mdo</td>
-    </tr>
-    <tr>
-      <th scope="row">2</th>
-      <td>Jacob</td>
-      <td>Thornton</td>
-      <td>@fat</td>
-    </tr>
-    <tr>
-      <th scope="row">3</th>
-      <td colSpan={2}>Larry the Bird</td>
-      <td>@twitter</td>
+     {
+      categories?.map(c =>(
+        <td key={c._id}>{c.name}</td>
+      ))
+     }
     </tr>
   </tbody>
 </table>
