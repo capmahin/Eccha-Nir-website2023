b1dd84bfbec52e573722055c7f00e1103ac3bad4
Hawkeye <36890222+capmahin@users.noreply.github.com>
2023-04-09 01:58:16 +0600

    add name

--- client/src/pages/Admin/CreateCategory.js
+++ client/src/pages/Admin/CreateCategory.js
@@ -50,7 +50,7 @@ const CreateCategory = () => {
    
   const handleUpdate = async(e) =>{
     try {
-      const {data} = await axios.put(`/api/v1/category/update-category/${selected._id}`)
+      const {data} = await axios.put(`/api/v1/category/update-category/${selected._id}`,{ name: updatedName })
     } catch (error) {
       console.log(error)
       toast.error("Somtihing went wrong");
