ee12979006e1817e13922185ddcf4f5535f56e66
Hawkeye <36890222+capmahin@users.noreply.github.com>
2023-04-10 14:50:47 +0600

    working on category page

--- client/src/pages/Admin/CreateProduct.js
+++ client/src/pages/Admin/CreateProduct.js
@@ -1,8 +1,18 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react'
 import AdminMenu from "../../components/Layout/AdminMenu"
-import Layout from "../../components/Layout/Layout"
+import Layout from "../../components/Layout/Layout";
+import axios from "axios";
+import toast from 'react-hot-toast'
 
 const CreateProduct = () => {
+  const [categories, setCategories] = useState([]);
+  const [name, setName] = useState("");
+  const [description, setDescription] = useState("");
+  const [price, setPrice] = useState("");
+  const [category, setCategory] = useState("");
+  const [quantity, setQuantity] = useState("");
+  const [shipping, setShipping] = useState("");
+  const [photo, setPhoto] = useState("");
   return (
     <Layout title={'Dashboard-Create Product'}>
       <div className="container-fluid m-3 p-3">
