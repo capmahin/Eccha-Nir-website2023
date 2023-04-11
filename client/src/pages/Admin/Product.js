0f8fba5d80b9fe902d2c22dd24b02085d3ddf0fd
Hawkeye <36890222+capmahin@users.noreply.github.com>
2023-04-12 01:36:04 +0600

    add product page

---
+++ client/src/pages/Admin/Product.js
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from "react";
+import AdminMenu from "../../components/Layout/AdminMenu";
+import Layout from "./../../components/Layout/Layout";
+import axios from "axios";
+import toast from "react-hot-toast";
+import { Link } from "react-router-dom";
+const Product = () => {
+  return (
+    <div>Product</div>
+  )
+}
+
+export default Product
