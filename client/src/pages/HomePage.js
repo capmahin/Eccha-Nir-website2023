b656445202ff4b5da753a5005bfd6851a9d606bf
Hawkeye <36890222+capmahin@users.noreply.github.com>
2023-04-13 01:38:35 +0600

    add

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,9 +1,34 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
 import Banner from "../components/Layout/Banner";
 import Layout from "../components/Layout/Layout"
 import { useAuth } from "../context/auth" 
+import axios from "axios";
+import { Checkbox, Radio } from "antd";
 const HomePage = () => {
-  const [auth,setAuth] = useAuth();
+  const [products, setProducts] = useState([]);
+  const [categories, setCategories] = useState([]);
+  const [checked, setChecked] = useState([]);
+  const [radio, setRadio] = useState([]);
+  const [total, setTotal] = useState(0);
+  const [page, setPage] = useState(1);
+  const [loading, setLoading] = useState(false);
+
+  //get all cat
+  const getAllCategory = async () => {
+    try {
+      const { data } = await axios.get("/api/v1/category/get-category");
+      if (data?.success) {
+        setCategories(data?.category);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getAllCategory();
+    getTotal();
+  }, []);
   return (
     <Layout>
       <Banner/>
