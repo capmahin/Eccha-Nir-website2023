e4a1b405617047687629f66f853ab69cdfa4dbb5
Hawkeye <36890222+capmahin@users.noreply.github.com>
2023-04-13 02:43:15 +0600

    add filter

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -49,6 +49,13 @@ const HomePage = () => {
        <div className="row mt-3">
         <div className="col-md-3">
           <h6>Search By Category</h6>
+          {
+            categories?.map((c)=>(
+              <Checkbox key={c._id} onChange={(e)=> console.log(e)}>
+                {c.name}
+              </Checkbox>
+            ))
+          }
         </div>
         <div className="col-md-9">
           <h1 className="text-center">All Products</h1>
