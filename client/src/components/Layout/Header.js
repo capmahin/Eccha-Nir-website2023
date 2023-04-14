798c31d7675ffd52638263ef90474962e3090f69
Hawkeye <36890222+capmahin@users.noreply.github.com>
2023-04-15 00:27:58 +0600

    add more categories

--- client/src/components/Layout/Header.js
+++ client/src/components/Layout/Header.js
@@ -1,17 +1,20 @@
 import React from 'react'
 import { NavLink,Link } from "react-router-dom"
-
+import toast from "react-hot-toast";
 import { useAuth } from "../../context/auth";
 import Ecche from '../../assists/Image/Eccha-nir.jpg'
 import SearchInput from "../Form/SearchInput";
+import useCategory from "../../hooks/useCategory";
 
 const Header = () => {
   const [auth,setAuth] = useAuth();
+  const categories = useCategory();
   const handleLogout = () =>{
     setAuth({
       ...auth, user:null,token:'',
     })
     localStorage.removeItem('auth')
+    toast.success("Logout Successfully");
   }
   return (
     <>
@@ -30,9 +33,33 @@ const Header = () => {
         <li className="nav-item">
           <NavLink to="/"  className="nav-link " >Home</NavLink>
         </li>
-        <li className="nav-item">
-          <NavLink to="/category"  className="nav-link " >Category</NavLink>
-        </li>
+        
+        <li className="nav-item dropdown">
+                <Link
+                  className="nav-link dropdown-toggle"
+                  to={"/categories"}
+                  data-bs-toggle="dropdown"
+                >
+                  Categories
+                </Link>
+                <ul className="dropdown-menu">
+                  <li>
+                    <Link className="dropdown-item" to={"/categories"}>
+                      All Categories
+                    </Link>
+                  </li>
+                  {categories?.map((c) => (
+                    <li>
+                      <Link
+                        className="dropdown-item"
+                        to={`/category/${c.slug}`}
+                      >
+                        {c.name}
+                      </Link>
+                    </li>
+                  ))}
+                </ul>
+              </li>
        {
         !auth?.user ? (
           <>
