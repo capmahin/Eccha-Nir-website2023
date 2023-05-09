51f9af7c77a61c987ae2910f7ce77b5d854e04bb
Hawkeye <36890222+capmahin@users.noreply.github.com>
2023-05-10 01:19:57 +0600

    one

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(morgan('dev'));
-app.use(express.static(path.join(__dirname, './client/build')))
+// app.use(express.static(path.join(__dirname, './client/build')))
 
 //routes
 app.use('/api/v1/auth',authRoutes);
@@ -30,12 +30,12 @@ app.use('/api/v1/category',categoryRoutes);
 app.use('/api/v1/product',productRoutes);
 
 // rest api
-app.use('*',function(req,res){
-    res.sendFile(path.join(__dirname,'./client/build/index.html'))
-})
+// app.use('*',function(req,res){
+//     res.sendFile(path.join(__dirname,'./client/build/index.html'))
+// })
 
 //PORT
-const PORT = process.env.PORT || 9000;
+const PORT = process.env.PORT || 7000;
 
 
 
