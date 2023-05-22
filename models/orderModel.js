589f42d8c99f838a824bcde994061bf6ebc8b877
Hawkeye <36890222+capmahin@users.noreply.github.com>
2023-05-23 02:32:21 +0600

    add orderModel

---
+++ models/orderModel.js
@@ -0,0 +1,53 @@
+import mongoose from "mongoose";
+
+const orderSchema = new mongoose.Schema({
+
+    name:{
+        type:String,
+        required:true
+    },
+    slug:{
+        type:String,
+        required:true
+    },
+    email:{
+        type:String,
+        required:true
+    },
+    phone:{
+        type:Number,
+        required:true
+    },
+    bkash:{
+        type:Number,
+        required:true
+    },
+    category:{
+        type:mongoose.ObjectId,
+        ref:'Category',
+        required:true
+    },
+    quantity:{
+        type:String,
+        required:true
+    },
+    address:{
+        type:String,
+        required:true
+    },
+    currieraddress:{
+        type:String,
+        required:true
+    },
+    size:{
+        type:String,
+        required:true
+    },
+    
+    shipping:{
+        type:Boolean
+    }
+
+},{timestamps:true})
+
+export default mongoose.model('Orders', orderSchema)
