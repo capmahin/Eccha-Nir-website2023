2e2169361b70c3a149e1ead6ee6ac2d5484b13e6
Hawkeye <36890222+capmahin@users.noreply.github.com>
2023-07-26 02:14:43 +0600

    add new img

--- client/src/pages/Expolre.js
+++ client/src/pages/Expolre.js
@@ -1,20 +1,22 @@
 import React from 'react' 
 import pant1 from "../assists/Explore/pant-1.avif" 
 import pant2 from "../assists/Explore/pant-2.avif" 
+import pant3 from "../assists/Explore/pant-3.avif" 
 import shirt1 from "../assists/Explore/t-shirt1.avif" 
 import shirt2 from "../assists/Explore/t-shirt2.avif" 
+import shirt3 from "../assists/Explore/t-shirt3.avif" 
 
 
 const Expolre = () => {
   return (
         
-        <div className="container p-2 m-2">
+        <div className="container p-5 m-5">
           <h1 className="text-center text-success show p-2">""Explore""</h1>
-          <div className="row  p-4 m-4">
+          <div className="row  p-5 m-5">
                 {/*Start Explore  */}
                 {/* First Exploring */}
 
-                <div className="col-md-6">
+                <div className="col-md-4">
                 <div className="card" style={{width: '18rem'}}>
   <img src={pant1} className="card-img-top" alt="..." height="250px" />
   <div className="card-body">
@@ -24,10 +26,20 @@ const Expolre = () => {
 {/* End First Exploring */}
                 </div>
                 
-                <div className="col-md-6">
+                <div className="col-md-4">
                  {/* 2nd Exploring */}
 <div className="card" style={{width: '18rem'}}>
   <img src={pant2} className="card-img-top" alt="..." height="250px" />
+  <div className="card-body">
+    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+  </div>
+</div>
+{/*  End 2nd Exploring */}
+                </div>
+                <div className="col-md-4">
+                 {/* 2nd Exploring */}
+<div className="card" style={{width: '18rem'}}>
+  <img src={pant3} className="card-img-top" alt="..." height="250px" />
   <div className="card-body">
     <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
   </div>
@@ -41,8 +53,8 @@ const Expolre = () => {
 
                 {/* End Explore */}
           </div>
-          <div className="row p-4 m-4">
-              <div className="col-md-6">
+          <div className="row p-5 m-5">
+              <div className="col-md-4">
                 {/* 3rd Explore */}
 <div className="card" style={{width: '18rem'}}>
   <img src={shirt1} className="card-img-top" alt="..." height="250px" />
@@ -53,7 +65,7 @@ const Expolre = () => {
 {/*  End 3rd Explore */}
 
               </div>
-              <div className="col-md-6">
+              <div className="col-md-4">
              {/* 4th Explore */}
 <div className="card" style={{width: '18rem'}}>
   <img src={shirt2} className="card-img-top" alt="..." height="250px" />
@@ -62,6 +74,17 @@ const Expolre = () => {
   </div>
 </div>
 {/*  End 4ths Explore */}
+
+              </div>
+              <div className="col-md-4">
+             {/* 5th Explore */}
+<div className="card" style={{width: '18rem'}}>
+  <img src={shirt3} className="card-img-top" alt="..." height="250px" />
+  <div className="card-body">
+    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+  </div>
+</div>
+{/*  End 5ths Explore */}
 
               </div>
           </div>
