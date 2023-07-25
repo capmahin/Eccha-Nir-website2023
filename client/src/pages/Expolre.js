71eab83720c47f17e9bbdb8b97aeaa42c91123db
Hawkeye <36890222+capmahin@users.noreply.github.com>
2023-07-26 01:52:13 +0600

    working on card ssection eplore

--- client/src/pages/Expolre.js
+++ client/src/pages/Expolre.js
@@ -6,32 +6,44 @@ const Expolre = () => {
         <div className="container p-2 m-2">
           <h1 className="text-center text-success show p-2">""Explore""</h1>
           <div className="row">
-            <div className="col-md-4">
-            <h3 className="bg-success p-3 text-white text-center rounded">Mock up your t-shirts</h3>
-            <div>
-          <h5 className="text-justify mt-2 p-2">Once you have some final designs, your next step for how to start a t-shirt business online will be to get some mockup images. Mockups are helpful so your customers can see what the final design will look like printed on a shirt.
+                {/*Start Explore  */}
+                {/* First Exploring */}
+                <div className="card" style={{width: '18rem'}}>
+  <img src="..." className="card-img-top" alt="..." />
+  <div className="card-body">
+    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+  </div>
+</div>
+{/* End First Exploring */}
 
-There are a few ways you can get images of your final designs on t-shirts, including ordering samples and taking product photography yourself or, with the abundance of t-shirt mockups and templates online</h5>
-        </div>
-        <hr />
-            </div>
-           
-            <div className="col-md-4">
-            <h3 className="bg-success p-3 text-white text-center rounded">Validate the designs for your t-shirt online business</h3>
-            <div><h5 className="text-justify mt-2 p-2">Before you invest a lot of money on your new business idea, you should verify that there is a market for and interest in your designs. There are many ways you can go about validating the market and sell t-shirts online.
+{/* 2nd Exploring */}
+                <div className="card" style={{width: '18rem'}}>
+  <img src="..." className="card-img-top" alt="..." />
+  <div className="card-body">
+    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+  </div>
+</div>
+{/*  End 2nd Exploring */}
 
-Some of the most popular and more common methods for validating your design ideas include:</h5></div>
-<hr />
-            </div>
-            
-            <div className="col-md-4">
-           <h3 className="bg-success p-3 text-white text-center rounded">T-shirt printing techniques</h3>
-           <h5 className="text-justify mt-2 p-2">There are three popular methods for printing when it comes to how to make an online t-shirt business. Each method has its pros and cons and deciding which one to use will partially depend on how much time you want to invest in the product creation and which printing partner you choose.
+{/* 3rd Explore */}
+                <div className="card" style={{width: '18rem'}}>
+  <img src="..." className="card-img-top" alt="..." />
+  <div className="card-body">
+    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+  </div>
+</div>
+{/*  End 3rd Explore */}
+{/* 4th Explore */}
+                <div className="card" style={{width: '18rem'}}>
+  <img src="..." className="card-img-top" alt="..." />
+  <div className="card-body">
+    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+  </div>
+</div>
+{/*  End 4th Explore */}
 
-Below, we have outlined all three t-shirt printing options, to give you a better understanding of each process. Whether you invest in your own printer or use a local supplier, it’s worth knowing how each printing technique works.</h5>
-<hr />
-            </div>
-            
+
+                {/* End Explore */}
           </div>
         </div>
        
